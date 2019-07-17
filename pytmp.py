156526c89e878ea8ef8c767c78726dabd1df6240
it4csl <it4csl@gmail.com>
2019-07-18 02:44:43 +0300

    test

--- pytmp.py
+++ pytmp.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from multiprocessing import Pool
+# from multiprocessing import Pool
 
-def f(x):
-    return x*x
+# def f(x):
+#     return x*x
 
-if __name__ == '__main__':
-    with Pool(5) as p:
-        print(p.map(f, [1, 2, 3]))
+# if __name__ == '__main__':
+#     with Pool(5) as p:
+#         print(p.map(f, [1, 2, 3]))
+
+az = [q, w, e, r, t, y, u, i]
+
+for s in az:
+    print(s)
